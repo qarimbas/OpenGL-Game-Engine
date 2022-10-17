d6f3d5a2d8d65a0735ba71d36ed4ab8f140fec7d
Baris Kerem Kurt <bariskerem.kurt@gmail.com>
2022-10-17 20:06:28 +0300

    Specular Lighting

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -28,12 +28,15 @@ public class MainGameLoop {
 
 
 
-        RawModel model = OBJLoader.loadObjModel("stall", loader);
-        ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
-        TexturedModel staticModel = new TexturedModel(model, texture);
+        RawModel model = OBJLoader.loadObjModel("dragon", loader);
+        //ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
+        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("white")));
+        ModelTexture texture = staticModel.getTexture();
+        texture.setShineDamper(10);
+        texture.setReflectivity(1);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0,0,-50), 0,0,0,1);
-        Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1,1,1));
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-25), 0,0,0,1);
+        Light light = new Light(new Vector3f(200,200,100), new Vector3f(1,1,1));
 
         Camera camera = new Camera();
 
